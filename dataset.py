d4adb483d8e638d1ed23a16323ee623c4be41d13
philiphan0109 <51898400+philiphan0109@users.noreply.github.com>
2023-12-21 13:03:46 -0500

    push

---
+++ dataset.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset, DataLoader
+
+class TimeSeriesDataset(Dataset):
+    def __init__(self, input_tensors, target_tensors):
+        self.input_tensors = input_tensors
+        self.target_tensors = target_tensors
+
+    def __len__(self):
+        return len(self.input_tensors)
+
+    def __getitem__(self, idx):
+        return self.input_tensors[idx], self.target_tensors[idx]
+
